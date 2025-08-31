71a3959224f2eda3f2fc74a1c79e1ac932d0a495
soumitrabanerjee <sama_664@yahoo.co.in>
2025-08-31 01:15:13 -0400

    inital project

---
+++ src/main/java/com/soumitra/axon/axon_eample/AxonEampleApplication.java
@@ -0,0 +1,13 @@
+package com.soumitra.axon.axon_eample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonEampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonEampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soumitra/axon/axon_eample/AxonEampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumitra.axon.axon_eample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonEampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
